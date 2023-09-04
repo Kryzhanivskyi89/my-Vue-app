e974a0861e7b0e2998c5cb3b4d38920606a57ad4
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-09-04 17:29:23 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script >
+  import Button from './components/Button.vue';
+  import DirectivesTest from './components/DirectivesTest.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Button,
+    DirectivesTest
+  },
+  data() {
+    return {
+      amountOfClicks: 10
+    }
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClicks}`
+    }
+  },
+  methods: {
+    increment() {
+      this.amountOfClicks += 1
+    }
+  }
+}
+
+
+</script>
+
+<template>
+ <div :id="$style.app">
+    <Button @click="increment">Click me</Button>
+    <h1>{{ title }}</h1>
+    <DirectivesTest/>
+  </div>
+</template>
+
+
+
+<style  module>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/DirectivesTest.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="isVisible">
+        <h1 v-show="false">Directives</h1>
+        <ul>
+            <template v-for="fruit in fruits">
+                <li v-if="fruit === 'apple'" :key="fruit">{{ fruit }}</li>
+            </template>
+        </ul>
+        <ul>
+            <li v-for="car in cars" :key="car.color">{{ car.color }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DirectivesTest',
+        data() {
+            return {
+                isVisible: true,
+                fruits: ['kiwi', 'banana', 'apple'],
+                cars: {
+                    audi: {
+                        color: 'red'
+                    },
+                    bmw: {
+                        color:'black'
+                    }
+                } 
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
